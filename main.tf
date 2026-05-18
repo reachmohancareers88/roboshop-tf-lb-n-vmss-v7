bcd299b8a5b3b80716cfa2567c1382cbb34467a6
Raghu K <raghuk.vit@gmail.com>
2026-05-18 07:19:29 +0530

    RoboShop Code

--- main.tf
+++ main.tf
@@ -12,36 +12,36 @@ module "db" {
   vm_count  = 1
 }
 
-#
-# module "apps" {
-#   depends_on = [module.db]
-#   source     = "./modules/vmss"
-#
-#   for_each       = var.apps
-#   component_name = each.key
-#   port           = each.value["port"]
-#
-#   lb_type = "private"
-#
-#   env       = var.env
-#   image_id  = var.image_id
-#   rgname    = var.rgname
-#   subnet_id = var.subnet_id
-# }
-#
-#
-# module "ui" {
-#   depends_on = [module.apps]
-#   source     = "./modules/vmss"
-#
-#   for_each       = var.ui
-#   component_name = each.key
-#   port           = each.value["port"]
-#
-#   lb_type = "public"
-#
-#   env       = var.env
-#   image_id  = var.image_id
-#   rgname    = var.rgname
-#   subnet_id = var.subnet_id
-# }
+
+module "apps" {
+  depends_on = [module.db]
+  source     = "./modules/vmss"
+
+  for_each       = var.apps
+  component_name = each.key
+  port           = each.value["port"]
+
+  lb_type = "private"
+
+  env       = var.env
+  image_id  = var.image_id
+  rgname    = var.rgname
+  subnet_id = var.subnet_id
+}
+
+
+module "ui" {
+  depends_on = [module.apps]
+  source     = "./modules/vmss"
+
+  for_each       = var.ui
+  component_name = each.key
+  port           = each.value["port"]
+
+  lb_type = "public"
+
+  env       = var.env
+  image_id  = var.image_id
+  rgname    = var.rgname
+  subnet_id = var.subnet_id
+}
